2aeea32f5ebc9b7c5a48af8f6fbe463441b4277d
PreethiECE <preethiece96@gmail.com>
2016-08-20 23:39:48 -0700

    Rename sumn.java to sum1.java

---
+++ sum1.java
@@ -0,0 +1,10 @@
+public static void main(String[] arg)
+{
+Scanner in=new Scanner(System.in)
+int n,sum=0;
+System.out.println("Enter number");
+n=in.nextInt();
+for(int i=0;i<=n;i++)
+sum+=i;
+System.out.println(sum);
+}
